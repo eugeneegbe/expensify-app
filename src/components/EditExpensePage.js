bb1fb91fd0e9db697a9a5182a23874664dfe426a
Egbe Eugene <agboreugene@gmail.com>
2020-07-23 11:38:47 +0100

    demo dynamic endpoints in edit component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => (
     <div>
-        Edit expense
+        Editing Expense with id: {props.match.params.id}
     </div>
 )
 
