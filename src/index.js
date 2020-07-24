004eada4b200539fa24487f6b24cc5167bc8989b
Egbe Eugene <agboreugene@gmail.com>
2020-07-24 09:58:05 +0100

    exploring sample reducers for expense app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import AppRouter from './routers/AppRouter';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore, combineReducers } from 'redux';
 
 
 ReactDOM.render(
@@ -15,3 +16,59 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const demoState = {
+  expenses:[
+    {
+      id: 'adskjfdashfjkdsaf',
+      description: 'January Rent',
+      note: 'Rents for the month of Jan',
+      amount: '5430',
+      createdAt: 0
+    }
+  ],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount', // date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+};
+
+const expenseReducerDefaultState = [];
+
+const expenseReducer = ( state = expenseReducerDefaultState, action) => {
+  switch (action.type) {
+    case '':
+      break;
+  
+    default:
+      return state;
+  }
+}
+
+const defaultFilterState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+
+const filterReducer = (state = defaultFilterState, action) => {
+  switch (action.type) {
+    case '':
+      break;
+  
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  combineReducers({
+    expenses: expenseReducer,
+    filters: filterReducer
+  })
+);
+
+console.log(store.getState())
