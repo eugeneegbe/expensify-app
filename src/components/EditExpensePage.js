1aa0c59b290a3d6d8ba5bd0a2d9cfb6421d3e967
Egbe Eugene <agboreugene@gmail.com>
2020-07-28 09:20:33 +0100

    Added datepicker with moment and react-dates

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from '../components/ExpenseForm';
 
 const EditExpensePage = (props) => (
     <div>
         Editing Expense with id: {props.match.params.id}
+        <ExpenseForm />
     </div>
 )
 
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+
+export default class ExpenseForm extends React.Component {
+        state = {
+        description: '',
+        note: '',
+        amount: '',
+        createdAt: moment(),
+        focusCalendar: false
+    }
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState( () => ({ description } ));
+    };
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState( () => ({ amount }))
+        }
+    };
+
+    onNoteChange = (e) =>{
+        const note = e.target.value;
+        this.setState( () => ({ note }))
+    };
+
+    onDateChage = (createdAt) => {
+        this.setState( () => ( { createdAt } ) );
+    };
+
+    onFocusChange = (focused) => {
+        this.setState( () => ( {focusCalendar: focused.focused} ) );
+    };
+
+    render(){
+        return(
+            <div>
+                <form>
+                    <input
+                        type='text'
+                        placeholder='Description'
+                        autoFocus={true}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <br></br>
+                    <input
+                        type='text'
+                        placeholder='Amount'
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    /><br></br>
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChage}
+                        focused={this.state.focusCalendar}
+                        onFocusChange={this.onFocusChange}
+                    />
+                    <textarea 
+                        onChange={this.onNoteChange}
+                        value={this.state.note}
+                        placeholder='Add a note for expense'
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        )
+    }
+}
