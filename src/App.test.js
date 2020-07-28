e9e08475ab588140a0da7a1e21a54211f76d6e07
Egbe Eugene <agboreugene@gmail.com>
2020-07-28 17:07:07 +0100

    Added test for editExpense and removeExpense

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// const generateGreeting = (name) => `Hello ${name}!`;
+
+// test('test Generate Greeting', () => {
+//   const greet = generateGreeting('Mike');
+//   expect(greet).toBe('Hello Mike!');
+// });
---
+++ src/tests/actions/expenses/expenses.test.js
@@ -0,0 +1,36 @@
+import addExpense from '../../../actions/expenses/addExpense';
+import removeExpense from '../../../actions/expenses/removeExpense';
+import editExpense from '../../../actions/expenses/editExpense';
+
+import {v4 as uuid} from 'uuid';
+
+test('Should setup removeExpense action object', () => {
+    const result = removeExpense( '123abc' );
+    expect(result).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('Should setup editExpense action object', () => {
+    const result = editExpense('123abc',{note: 'New note value'});
+    expect(result).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {note: 'New note value'}
+    })
+})
+
+// test('Should setup addExpenses action object', () => {
+//     const result = addExpense({ description:'Test', note:'Test note', amount: 200, createdAt: 1000 })
+//     expect(result).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             id: '123abc',
+//             description: 'Test',
+//             note:'Test note',
+//             amount: 200,
+//             createdAt: 1000
+//         }
+//     });
+// });
