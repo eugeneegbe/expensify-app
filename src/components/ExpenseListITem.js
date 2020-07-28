4a799b31e1504c5e78e54548ce2d876cd8e972ed
Egbe Eugene <agboreugene@gmail.com>
2020-07-28 10:21:19 +0100

    Added link to individual edit item page

--- src/components/ExpenseListITem.js
+++ src/components/ExpenseListITem.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import removeExpense from '../actions/expenses/removeExpense';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
-        <p>{description} - ${amount} | {createdAt}</p>
+        <Link to={'/edit/'.concat(id)}>{description}</Link>
+        
+        <p>${amount} | {createdAt}</p>
         <button
-            onClick={ () => {
-                
-                dispatch(removeExpense({ id }))
-            } }
+            onClick={ () => {dispatch(removeExpense({ id }))} }
         >Remove</button>
     </div>
 )
