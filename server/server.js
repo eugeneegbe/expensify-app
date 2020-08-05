ea731d3303f96a4e3f537d70ea66160954dfbb25
Egbe Eugene <agboreugene@gmail.com>
2020-08-05 11:42:42 +0100

    Added server configs for heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require( 'path' );
 const express = require( 'express' );
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 300;
 
 app.use( express.static( publicPath ) );
 
@@ -9,6 +10,6 @@ app.get( '*', ( req, res ) => {
     req.sendFile( path.join( publicPath, 'index.html' ) );
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log( 'Server started!' );
 });
