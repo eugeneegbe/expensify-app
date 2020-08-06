56c433a78123ce587650d2e418dad6944d8f8ca1
Egbe Eugene <agboreugene@gmail.com>
2020-08-06 14:16:59 +0100

    Added export for firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,9 +4,24 @@ import firebaseConfig from './config';
 firebase.initializeApp(firebaseConfig);
 
 firebase.database().ref().set( {
-    name: 'New deal'
+    name: 'New deal',
+    age: 27,
+    isSingle: false,
+    location: {
+        city: 'Yaounde',
+        Country: 'Cameroon'
+    }
 } ).then( () => {
     console.log( 'Data added successfully!' )
 } ).catch( ( e ) => {
     console.log( 'Something went wrong!', e)
 } );
+
+var adaRef = firebase.database().ref( 'isSingle' );
+adaRef.remove()
+  .then(function() {
+    console.log("Remove succeeded.")
+  })
+  .catch(function(error) {
+    console.log("Remove failed: " + error.message)
+  });
