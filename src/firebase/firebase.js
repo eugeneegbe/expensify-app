40d840723c005880eda146220da81c154c895eb0
Egbe Eugene <agboreugene@gmail.com>
2020-08-06 13:53:50 +0100

    Added promise feedback from firebase operation

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,6 +3,10 @@ import firebaseConfig from './config';
 
 firebase.initializeApp(firebaseConfig);
 
-firebase.database().ref().set({
+firebase.database().ref().set( {
     name: 'New deal'
-})
+} ).then( () => {
+    console.log( 'Data added successfully!' )
+} ).catch( ( e ) => {
+    console.log( 'Something went wrong!', e)
+} );
