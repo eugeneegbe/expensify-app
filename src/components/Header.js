d329d76e4eadbd5c04b91e89236858905a90263f
Egbe Eugene <agboreugene@gmail.com>
2020-08-10 14:00:24 +0100

    Style header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,14 @@ import {Link, NavLink} from 'react-router-dom';
 const Header = () => (
     <div>
         <div className='header'>
-            <Link to="/" className='header__title' exact={true}>
-                <h1>Expensify</h1>
-            </Link>
-            <NavLink to="/help" activeClassName='is-active'>Help</NavLink>
+            <div className='header-container'>
+                <div className='header__content'>
+                    <Link to="/" className='header__title' exact={true}>
+                        <h1>Expensify</h1>
+                    </Link>
+                    <NavLink to="/help" activeClassName='is-active'>Help</NavLink>
+                </div>
+            </div>
         </div>
     </div>
 );
